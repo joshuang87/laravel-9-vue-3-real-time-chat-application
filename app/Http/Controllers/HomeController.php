2b94bfed9c9ae16dd48795fa4637809cb9349fc3
joshuang87 <46678886+joshuang87@users.noreply.github.com>
2022-12-15 23:15:45 +0800

    Add files via upload

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\SendMessage;
+use App\Models\Message;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function chat(){
+        return view('chat');
+    }
+
+    public function messages(){
+        return Message::with('user')->get();
+    }
+
+    public function messageStore(Request $request){
+        $user=Auth::user();
+
+        $messages=$user->messages()->create([
+            'message'=>$request->message
+        ]);
+
+        broadcast(new SendMessage($user,$messages))->toOthers();
+
+        return 'message sent';
+    }
+}
